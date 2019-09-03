a471b45cfe39bf07d3fc71159f85ee996800f6b2
Mohanad Elhag <moh.7afiz@gmail.com>
2019-09-03 22:52:02 +0300

    created context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
+import {
+  ProjectsProvider,
+  SelectedProjectProvider
+  // SelectedProjectProvider
+} from "./context";
 
 export const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
   );
 };
